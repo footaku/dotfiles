17ae01fe1f61b80b871a289ee813e8079d6f8baa
fu-taku <age.of.pan+github@gmail.com>
2018-02-23 19:01:01 +0900

    Add brew script

---
+++ .script/brew-update.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+printf '\n\033[1;36m### update \033[m\n' && \
+  brew update && \
+  printf '\n\033[1;36m### upgrade \033[m\n' && \
+  brew upgrade && \
+  printf '\n\033[1;36m### prune \033[m\n' && \
+  brew prune && \
+  printf '\n\033[1;36m### cleanup \033[m\n' && \
+  brew cleanup && \
+  printf '\n\033[1;36m### cask cleanup \033[m\n' && \
+  brew cask cleanup && \
+  printf '\n\033[1;36m### doctor \033[m\n' && \
+  brew doctor
+
+exit 0
